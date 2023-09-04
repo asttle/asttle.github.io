b06816fd7058020e30b483bfc1ce02ddcd0a3151
asttle <asttle1997@gmail.com>
2023-09-04 13:38:06 +0100

    changes completed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,15 +8,16 @@ module.exports = {
   deploy: {
     production: {
       key: "portfoliokey.pem",
-      user: "linux",
+      user: "ec2-user",
       host: "18.170.39.157",
       ref: "origin/main",
       repo: "git@github.com:asttle/asttle.github.io.git",
-      path: "/home/ec2-user",
+      path: "/home/ec2-user/AsttlePersonalSite",
       "pre-deploy-local": "",
       "post-deploy":
         "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
       "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
     },
   },
 };
